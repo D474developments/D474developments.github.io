6db1d690c3daf43fc34ad252433624853b26c78e
Jaime Orlando Cazares Valdez <40035675+D474designs@users.noreply.github.com>
2023-09-22 14:05:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -41,17 +41,16 @@ var Boid = function ( x, y, angle ) {
 
 }
 
-
+/*
 var body = document.body,
     html = document.documentElement;
 
 var height = Math.max( body.scrollHeight, body.offsetHeight,
                        html.clientHeight, html.scrollHeight, html.offsetHeight );
+*/
 
-/*
 var width = window.innerWidth;
 var height = window.innerHeight;
-*/
 
 var canvas = document.getElementById( 'world' );
 canvas.width = width;
