45968013c42fdb326840654859a30bb63a786481
Jaime Orlando Cazares Valdez <40035675+D474designs@users.noreply.github.com>
2023-09-22 14:37:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -49,8 +49,8 @@ var height = Math.max( body.scrollHeight, body.offsetHeight,
                        html.clientHeight, html.scrollHeight, html.offsetHeight );
 */
 
-var width = window.width;
-var height = window.height;
+var width = window.screen.width;
+var height = window.screen.height;
 
 /*
 var width = window.innerWidth;
